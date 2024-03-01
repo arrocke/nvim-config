1e297b708049ee59e65db1666803bcb315034fb5
Adrian Rocke <me@adrianrocke.com>
2024-03-01 12:48:45 -0600

    feat: remaps

--- lua/test/init.lua
+++ lua/test/init.lua
@@ -4,5 +4,51 @@ require("test.lazy")
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Paste without losing clipboard
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Copy to clipboard
+vim.keymap.set('n', "<leader>y", "\"+y")
+vim.keymap.set('v', "<leader>y", "\"+y")
+vim.keymap.set('n', "<leader>Y", "\"+Y")
+
+vim.keymap.set('n', "<leader>d", "\"_d")
+vim.keymap.set('v', "<leader>d", "\"_d")
+
+vim.keymap.set('n', "Q", "<nop>")
+
+vim.keymap.set('n', "<leader>f", function() vim.lsp.buf.format() end)
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+
 vim.cmd("colorscheme rose-pine")
 
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append('@-@')
+
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
