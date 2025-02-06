1d7bbd1d5addffd57699425a672937f742fd0186
Adrian Rocke <adrian.rocke@webflow.com>
2025-02-06 10:31:17 -0600

    fix: find window for running webflow integration tests

--- lua/test/webflow.lua
+++ lua/test/webflow.lua
@@ -2,6 +2,7 @@ local serverMochaSuffix = '_test.ts'
 local termWindow
 vim.keymap.set('n', '<Leader>rt', function()
     local path = vim.api.nvim_buf_get_name(0)
+    local currentWin = vim.api.nvim_get_current_win()
     if string.sub(path, -#serverMochaSuffix) == serverMochaSuffix then
         if termWindow and vim.api.nvim_win_is_valid(termWindow) then
             vim.api.nvim_set_current_win(termWindow)
@@ -11,5 +12,6 @@ vim.keymap.set('n', '<Leader>rt', function()
             termWindow = vim.api.nvim_get_current_win()
         end
         vim.cmd("term ./bin/babel-mocha " .. path)
+        vim.api.nvim_set_current_win(currentWin)
     end
 end)
