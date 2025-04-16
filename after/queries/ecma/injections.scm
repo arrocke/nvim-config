24f3f949bf06ba89b9ca39697e402727fdc790f6
Adrian Rocke <me@adrianrocke.com>
2025-04-16 18:06:24 -0500

    feat: add more sql coloring injections

--- after/queries/ecma/injections.scm
+++ after/queries/ecma/injections.scm
@@ -4,7 +4,7 @@
 ; foo(/* sql */ `SELECT * FROM foo`)
 ((
   (template_string) @constant
-  (#match? @constant "(SELECT|select|insert|INSERT).*")
+  (#match? @constant "(SELECT|select|insert|INSERT|DELETE|delete|WITH|with|UPDATE|update).*")
 )@injection.content (#set! injection.language "sql") 
     (#set! injection.include-children))
 
