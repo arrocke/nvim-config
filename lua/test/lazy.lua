4fbed54b40a029c8ac1b11ef6f10ab2ed5f93e4c
Adrian Rocke <adrian.rocke@webflow.com>
2025-06-10 10:09:15 -0500

    webflow improvements

--- lua/test/lazy.lua
+++ lua/test/lazy.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -26,5 +26,6 @@ require("lazy").setup({
 	"hrsh7th/cmp-nvim-lsp",
 	"L3MON4D3/LuaSnip",
     { 'kevinhwang91/nvim-ufo', dependencies = { 'kevinhwang91/promise-async' } },
-    { 'nvim-treesitter/nvim-treesitter-context' }
+    { 'nvim-treesitter/nvim-treesitter-context' },
+    { 'augmentcode/augment.vim' },
 })
--- lua/test/webflow.lua
+++ lua/test/webflow.lua
@@ -1,9 +1,10 @@
 local serverMochaSuffix = '_test.ts'
+local serverMochaSuffixJs = '_test.js'
 local termWindow
 vim.keymap.set('n', '<Leader>rt', function()
     local path = vim.api.nvim_buf_get_name(0)
     local currentWin = vim.api.nvim_get_current_win()
-    if string.sub(path, -#serverMochaSuffix) == serverMochaSuffix then
+    if string.sub(path, -#serverMochaSuffix) == serverMochaSuffix or string.sub(path, -#serverMochaSuffixJs) == serverMochaSuffixJs then
         if termWindow and vim.api.nvim_win_is_valid(termWindow) then
             vim.api.nvim_set_current_win(termWindow)
         else
