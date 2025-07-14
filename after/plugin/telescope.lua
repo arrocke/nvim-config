9c9c7013fe24f04ff05b4ce1ba0f7b78338a32d6
Adrian Rocke <adrian.rocke@webflow.com>
2025-07-14 12:40:35 -0500

    telescope resume shortcut

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,3 +5,4 @@ vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 vim.keymap.set('n', '<leader>psl', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>pr', builtin.resume, {})
