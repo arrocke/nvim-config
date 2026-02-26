cbdabeb911e4f832a578edbcdd46953e0cf44d29
Adrian Rocke <me@adrianrocke.com>
2026-02-26 17:37:07 -0600

    replace nvim-cmp with blink.cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,30 +9,11 @@ return {
         end,
     },
     {
-        "hrsh7th/nvim-cmp",
-        tag = "v0.0.2",
-        opts = function()
-            local cmp = require('cmp')
-            return {
-                sources = {
-                    {name = 'nvim_lsp'},
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ['<CR>'] = cmp.mapping.confirm({select = false}),
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                }),
-                snippet = {
-                    expand = function(args)
-                      require('luasnip').lsp_expand(args.body)
-                    end,
-                },
-            }
-        end,
-        config = function()
-            vim.lsp.config('*', {
-                capabilities = require('cmp_nvim_lsp').default_capabilities()
-            })
-        end,
-        dependencies = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip" },
+        "saghen/blink.cmp",
+        tag = "v1.9.1",
+        opts = {
+            keymap = { preset = 'super-tab' },
+            fuzzy = { implementation = "lua" }
+        },
     },
 }
