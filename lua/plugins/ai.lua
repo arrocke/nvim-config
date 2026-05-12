e18ecbb2468607133f53174b68dd591bec3402c2
Adrian Rocke <me@adrianrocke.com>
2026-05-11 22:26:29 -0500

    ai, cmp

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -9,7 +9,7 @@ return {
 
             vim.keymap.set({ "n", "x" }, "<C-a>", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode" })
             vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
-            vim.keymap.set({ "n", "t" }, "<C-.>", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
+            vim.keymap.set({ "n", "t" }, "<C-t>", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
             vim.keymap.set({ "n", "x" }, "go",  function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
             vim.keymap.set("n",          "goo", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
             vim.keymap.set("n",        "<S-C-u>", function() require("opencode").command("session.half.page.up") end,   { desc = "opencode half page up" })
